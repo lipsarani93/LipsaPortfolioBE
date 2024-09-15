007974f132c576cb9fae0e46481cd98a089e5059
Lipsa <lrm9030@gmail.com>
2024-09-15 17:35:22 +0530

    fixed env

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Define the MongoDB connection URL
-// const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
-const mongoURL = "mongodb+srv://lipsarani:mpPM2Bh0BMdNkMHc@cf.vukko.mongodb.net/?retryWrites=true&w=majority&appName=CF";
+const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
+// const mongoURL = "";
 
 // Set up MongoDB connection
 mongoose.connect(mongoURL, {
